704233c96fe009b28b402f561c5d9c14109ef15b
Staneesh <blkreasons@outlook.com>
2020-10-07 14:56:08 +0200

    🏷️ Better types and utils library files

--- main.c
+++ main.c
@@ -1,5 +1,21 @@
+#include <stdio.h>
+#include <stdint.h>
 
-int main()
+#include "utils.c"
+
+typedef int8_t i8; 
+typedef int16_t i16; 
+typedef int32_t i32; 
+typedef int64_t i64; 
+typedef uint8_t u8; 
+typedef uint16_t u16; 
+typedef uint32_t u32; 
+typedef uint64_t u64; 
+typedef float real32;
+typedef double real64;
+
+int main(i32 argc, char** argv)
 {
+	
 	return 0;
 }	
