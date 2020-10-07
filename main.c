4309a7b1fc9e43b9d9a4752ccbcc897408a75bc9
Staneesh <blkreasons@outlook.com>
2020-10-07 15:25:44 +0200

    ✨ Debug marcos

--- main.c
+++ main.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
 #include <stdint.h>
 
+//NOTE(stanisz): requires stdio.h to be included before this line
 #include "utils.c"
-
-typedef int8_t i8; 
-typedef int16_t i16; 
-typedef int32_t i32; 
-typedef int64_t i64; 
-typedef uint8_t u8; 
-typedef uint16_t u16; 
-typedef uint32_t u32; 
-typedef uint64_t u64; 
-typedef float real32;
-typedef double real64;
 
 int main(i32 argc, char** argv)
 {
-	
+	LOG_INT(argc);	
+	LOG_POINTER(argv);
+	LOG_FLOAT(0.213);
+	LOG_STRING("ASDADS");
+	LOG_HERE;
+
 	return 0;
 }	
