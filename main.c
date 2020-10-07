fecbdbe519ab5d2dbd20122ea63dcb5b62985ac7
Staneesh <blkreasons@outlook.com>
2020-10-07 17:43:47 +0200

    ✨ Render strip function for future threading

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <math.h>
+#include <assert.h>
 
 //NOTE(stanisz): requires stdio.h to be included before this line
 #include "utils.c"
@@ -19,6 +20,46 @@ struct WorkQueue
 
 	struct WorkOrder* work_orders;
 };
+
+float lerp(float a, float b, float t)
+{
+	assert(t >= 0.0f && t <= 1.0f);
+		
+	return (1.0f - t) * a + b;
+}
+
+u32 get_pixel_color(u32 x, u32 y, u32 width, u32 height)
+{
+	float x_interp = lerp(0.0, 255.0, (float)x / width);
+	float y_interp = lerp(0.0, 255.0, (float)y / height);
+	u32 color = 0;
+
+	u32 red = 0;
+	u32 green = (u32)x_interp;
+	u32 blue = (u32)y_interp;
+
+	color |= (red);
+	color |= (green << 2);
+	color |= (blue << 4);
+
+	return color; 
+}
+
+void render_strip(struct WorkQueue* work_queue, u32* pixels, u32 width, u32 height)
+{
+	//interlocked add, return previous.
+	struct WorkOrder current = work_queue->work_orders[work_queue->next_work_order++];
+
+	for (u32 y = current.y_start; y <= current.y_end; ++y)
+	{
+		for (u32 x = 0; x < width; ++x)
+		{
+			u32 array_index = y * width + x;
+
+			pixels[array_index] = get_pixel_color(x, y, width, height);
+		}
+	}
+}
 
 int main(i32 argc, char** argv)
 {
